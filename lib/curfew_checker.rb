19dd0b16ea1e7cda19da1668b8b895943fafa98c
Jennifer Okwara <jokwara03@gmail.com>
2018-07-24 17:46:28 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -2,15 +2,15 @@ def simple_curfew_checker(time)
   if time > = 11 
     puts "should tell you if you're in trouble if it's 11"
 end
-end 
+
 
 def curfew_checker(time)
-   if time > = 11 
+   if time > = 12
      puts " you're in trouble if it's later than 11"
 end
 
 def complex_curfew_checker(time)
-  # code goes here
+  #if time > = 
 end
 
 def deluxe_curfew_checker(time)
